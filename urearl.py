574b2ebd8d6352bbb6a72bb6ce4c652f2e4bb9b1
nvz <72872161+enveezee@users.noreply.github.com>
2021-04-03 15:15:36 -0400

    Update urearl.py

--- urearl.py
+++ urearl.py
@@ -13,105 +13,94 @@ DEFAULT_URL_REGEX=None
 # Standard (Regular) Library imports.
 from html.parser import HTMLParser
 from json import loads
-from urllib.error import HTTPError
-from urllib.parse import urlparse
-from urllib.request import Request, urlopen
 from re import match
+from urllib.error import HTTPError
+from urllib.parse import unquote, urlparse
+from urllib.request import Request, urlopen
+from time import asctime, gmtime, strftime, time
+from sys import argv
 
-# Static globals method names, if you don't want to use string literals.
-D = DOWNLOAD = 'DOWNLOAD'
-H = HTML = 'HTML'
-J = JSON = 'JSON'
-P = PARSE = 'PARSE'
-R = REQUEST = 'REQUEST'
-T = TRIM = 'TRIM'
-V = VALIDATE = 'VALIDATE'
-
-class UREarl():
+class UREarl:
     '''UNCLE R. EARL class object.
     
     This class extracts returnable information from URLs using standard library
     methods with batteries included.
 
-    UREarl(url=None, method=None, headers=None)
-
-    Methods: DOWNLOAD, HTML, JSON, PARSE, REQUEST, TRIM, VALIDATE
+    UREarl(url=None, headers=None)
     '''
-    def __init__(self, url=None, method=None, headers=None):
-        # If instantiated with a URL, then set the URL for this instance.
-        if url:
-            self.url(url)
-            # If instantiated with a method, call the method(s).
-            if method:
-                func = getattr(self, method.casefold())
-                func()
+    def __init__(self, url, headers=None):
+        # Timestamp creation of URL instance.s
+        self.ctime = time()
+
+        # Set request headers.
+        self._headers = headers
+
+        # Parse out the URL for this instance.
+        self._url = urlparse(url)
+
+        # Make the request for this instance.
+        self.request()
+
+        # Handle response.
+        self.response()
+
+
+    def __getattribute__(self, name):
+        # Track when class object was last accessed.
+        if name[2:] != 'time':
+            super(URL, self).__setattr__('atime', time())
+        return super(URL, self).__getattribute__(name)
+
+
+    def __setattr__(self, name, value):
+        # Track when class object was last modified.
+        if name != '_atime':
+            super(URL, self).__setattr__('mtime', time())
+        super(URL, self).__setattr__(name, value)
 
 
     def download(self):
         '''Download a file from URL.'''
-        response = self.request(self.url)
         # ! NOT IMPLEMENTED YET
         # TODO: detect filename, open and write file.. etc
 
 
     def html(self):
-        '''Extract information from HTML.'''
-        response = self.request(self.url)
-        return htmlParser(response)
-
+        # ! NOT IMPLEMENTED
+        # ? detect if its actually html, or implement other parsers
+        pass
 
 
     def json(self):
         '''Parse JSON response from URL.'''
-        response = self.request(self.url)
-        if self.headers['Content-Type'] == 'application/json':
-            try:
-                jsonDict = loads(response)
+        # Try to parse JSON response.
+        try:
+            jsonDict = loads(self._response.read())
 
-            except JSONDecodeError as e:
-                print('JSON decoding failed!')
-                return e
+        # Handle errors with parsing
+        except JSONDecodeError as e:
+            self.error = 'JSON decoding failed!'
+            return e
 
-            return jsonDict
-        
-        else:
-            print('The content is not JSON')
-            return None
+        # Set JSON data.
+        self.data = jsonDict
 
 
-    def parse(self, *args):
-        '''Parse URL into its comoponent parts.
-        
-        If arguments are supplied, only those parts will be returned as a list.
-
-        Without arguments a ParseResult object will be returned.
-        '''
-        url = urlparse(self.url)
-        if args:
-            # Return requested parts.
-            output = []
-            for arg in args:
-                output.append(getattr(url, arg))
-            return output
-        # Return ParseResult.
-        return url
-
-
-    def request(self, headers=self.HEADERS):
+    def request(self):
         '''Form a request with optional headers for this url and attept to open
         returning a response.
         '''
-        request = Request(self.url)
-        if headers:
+        self._request = Request(self._url.geturl())
+        if self._headers:
             # Supplied headers.
-            for header in headers:
-                request.add_header(header, headears[header])
+            for header in self._headers:
+                self._request.add_header(header, self._headers[header])
         else:
             # Default headers.
-            request.add_header('User-Agent', DEFAULT_USER_AGENT)
+            self._request.add_header('User-Agent', DEFAULT_USER_AGENT)
 
         try:
-            response = urlopen(request)
+            self._response = urlopen(request)
         except HTTPError as e:
             # e.code, HTTP Error code.
             #   404
@@ -124,49 +113,40 @@ class UREarl():
             #   Date: Thu, 01 Apr 2021 04:31:31 GMT
             #   Connection: close
             # e.fp, pointer to the http.client.HTTPResponse object.
-            self.code = e.code
-            self.headers = e.hdr
-            self.msg = e.msg
-            self.response = e.fp
+            self.code = e.code      # HTTPError code
+            self.error = e.msg      # HTTPError message
+            self.headers = e.hdr    # HTTPError headers
+            self._response = e.fp   # HTTPResponse object
             return e
 
-        self.code = response.status
+        # Set HTTPResponse status code.
+        self.code = self._response.status
+        # Set error to None, to know we succeeded in making request
+        self.error = None
+        # Set HTTPResponse headers.
         self.headers = dict(response.getheaders())
-        self.response = response
 
-        if not self.url == response.url:
+        if self._url.geturl() != response.url:
             self.redirect = True
 
-        if read:
-            return response.read()
-        else:
-            return response
 
+    def response(self):
+        if not self.error:
+            contentType = self.headers['Content-Type']
+            if 'application/json' in contentType:
+                self.json()
+            elif 'text/html' in contentType:
+                # TODO hand off to self.html for further detection
+                self.data = htmlParser(self.response.read())
+            else:
+                self.download()
 
-    def trim(self):
-        '''Remove query strings from URL.'''
-        if '?' in self.url: # ? Too simplistic?
-            self.url = ''.join(self.url.split('?')[0])
-        return self.url
-
-
-    def url(self, url):
-        '''Set or change the URL for this instance.'''
-        self.url = url
-
-
-    def validate(self):
-        '''Check that URL is properly formed.'''
-        # ! NOT IMPLEMENTED YET
-        # TODO: check url against a regular expression, possibly encode it.
-        pass
-
+#! Below here lies fecal code not finished.
 
 class htmlParser(HTMLParser):
     '''A simple standard library HTML parser for extracting information.'''
     def __init__(self, html):
         super().__init__()
-        document = htmlDoc()
 
 
     def handle_starttag(self, tag, attrs):
@@ -181,12 +161,12 @@ class htmlParser(HTMLParser):
         pass
 
 
-class htmlDoc():
-    def __init__(self, url)
+class htmlDoc:
+    def __init__(self, html)
         pass
 
 
-class htmlTag():
+class htmlTag:
     def __init__(self, tag, parent):
         self.attrs = {}
         self.children = []
@@ -210,5 +190,6 @@ class htmlTag():
 
 
 if __name__ == '__main__':
-    # TODO: Parse commandline and handle URL
-    pass
+    # ! Braindead commandline implementation
+    # TODO write a more robust implementation of commandline usage.
+    print(URLEarl(argv[1]).data)
